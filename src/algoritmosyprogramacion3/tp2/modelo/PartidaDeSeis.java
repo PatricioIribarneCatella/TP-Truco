ec7efba0071665994d329991800f407e68e92479
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2015-12-06 01:23:11 -0300

    Modificación a la clase PartidaDeSeis hecha por Santiago Pinto.

--- src/algoritmosyprogramacion3/tp2/modelo/PartidaDeSeis.java
+++ src/algoritmosyprogramacion3/tp2/modelo/PartidaDeSeis.java
@@ -18,31 +18,38 @@ public class PartidaDeSeis extends Partida {
 	
 	private void initialize(List<String> nombresJugadoresEquipo1, List<String> nombresJugadoresEquipo2, boolean conFlor) {
 		
-		Mesa mesaDeDos;
+	    Mesa mesaDeSeis;
 		
 		List<Jugable> jugadores = new LinkedList<Jugable>();
 		
 		Equipo equipo1 = new Equipo();
-		
-		for (String nombreJugador : nombresJugadoresEquipo1) {
-			
-			Jugable jugador = new Jugador(nombreJugador);
-			equipo1.agregarIntegrante(jugador);
-			jugadores.add(jugador);
-		}
-		
 		Equipo equipo2 = new Equipo();
 		
-		for (String nombreJugador : nombresJugadoresEquipo2) {
-			
-			Jugable jugador = new Jugador(nombreJugador);
-			equipo2.agregarIntegrante(jugador);
-			jugadores.add(jugador);
-		}
+		Jugable jugador1 = new Jugador(nombresJugadoresEquipo1.get(0));
+		Jugable jugador2 = new Jugador(nombresJugadoresEquipo2.get(0));
+		Jugable jugador3 = new Jugador(nombresJugadoresEquipo1.get(1));
+		Jugable jugador4 = new Jugador(nombresJugadoresEquipo2.get(1));
+		Jugable jugador5 = new Jugador(nombresJugadoresEquipo1.get(2));
+		Jugable jugador6 = new Jugador(nombresJugadoresEquipo2.get(2));
 		
-		if (conFlor) mesaDeDos = new MesaConFlor(jugadores);
-		else mesaDeDos = new MesaSinFlor(jugadores);
-		this.moderador = new Moderador(mesaDeDos);
+		equipo1.agregarIntegrante(jugador1);
+		equipo1.agregarIntegrante(jugador3);
+		equipo1.agregarIntegrante(jugador5);
+		equipo2.agregarIntegrante(jugador2);
+		equipo2.agregarIntegrante(jugador4);
+		equipo2.agregarIntegrante(jugador6);
+		
+		jugadores.add(jugador1);
+		jugadores.add(jugador2);
+		jugadores.add(jugador3);
+		jugadores.add(jugador4);
+		jugadores.add(jugador5);
+		jugadores.add(jugador6);
+		
+		
+		if (conFlor) mesaDeSeis = new MesaConFlor(jugadores);
+		else mesaDeSeis = new MesaSinFlor(jugadores);
+		this.moderador = new Moderador(mesaDeSeis);
 		this.moderador.setPartida(this);
 		this.moderador.setRotacionStrategy(new StrategyRotacionEnRonda(jugadores));
 		
